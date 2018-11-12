3247e08316bcb795ca94609339092a36db860179
gabriel <duartega@sonoma.edu>
2018-11-12 13:00:08 -0800

    Need to get recursion and direction working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //Description: Takes a text file supplied by the user
 //             and turns it into a word index, implemented
-//             through the use of a BST 
+//             through the use of a BST
 
 #include <iostream>
 #include <fstream>
@@ -8,58 +8,64 @@
 #include "two-three.cpp"
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	int choice;
 	char first_choice;
-	if (argc != 2) {
-	    cout << "Incorrect input. Correct format: ./<exectuable.out> <inputtext.txt>\n";
-	    return 1;
+	if (argc != 2)
+	{
+		cout << "Incorrect input. Correct format: ./<exectuable.out> <inputtext.txt>\n";
+		return 1;
 	}
 
 	ifstream input(argv[1]);
-	BST myTree;
+	BST myBSTTree;
 	TTT tTree;
-	if(input.is_open()){
-	    tTree.buildTree(input);
-            myTree.buildTree(input);
+	if (input.is_open())
+	{tTree.buildTreeTTT(input);
+		myBSTTree.buildTree(input);
+		
 
-            input.close();
-            while(1){
-	        choice = 0;
-		cout << "Options: (a) BST, (b) 2-3 Tree, (c) Compare BST and 2-3 Tree\n";
-		cin >> first_choice;
-	        cout <<"Options: (1) display index, (2) search, (3) save index, (4) quit\n";
-	        cin >> choice;
+		input.close();
+		while (1)
+		{
+			choice = 0;
+			cout << "Options: (a) BST, (b) 2-3 Tree, (c) Compare BST and 2-3 Tree\n";
+			cin >> first_choice;
+			cout << "Options: (1) display index, (2) search, (3) save index, (4) quit\n";
+			cin >> choice;
 
-		//Print index
-	        if(choice == 1 && first_choice == 'b')
-	            tTree.printTree(cout);
-		else if (choice == 1 && first_choice == 'a')
-		    myTree.printTree(cout);
-	       
-		//Search index for a word
-		else if(choice == 2)
-	            myTree.contains();
+			//Print index
+			if (first_choice == 'b' && choice == 1)
+				tTree.printTreeTTT(cout);
+			else if (first_choice == 'a' && choice == 1)
+				myBSTTree.printTree(cout);
 
-		//Save index
-	        else if(choice == 3){
-		    string outputFile;
-	            cout << "Enter a filename to save your index to (Suggested: <filename>.txt) : ";
-		    cin >> outputFile;
-		    ofstream output(outputFile.c_str());
-	            myTree.printTree(output);
-	            output.close();
-		    cout << "Saved\n";
-	        }
+			//Search index for a word
+			else if (choice == 2)
+				myBSTTree.contains();
 
-		//Quit	
-	        else
-		    break;
-            }
-        }
-	else{
-	    cout << "Invalid File Name. Restart Program.\n";
-	    return 2;
-        }
+			//Save index
+			else if (choice == 3)
+			{
+				string outputFile;
+				cout << "Enter a filename to save your index to (Suggested: <filename>.txt) : ";
+				cin >> outputFile;
+				ofstream output(outputFile.c_str());
+				myBSTTree.printTree(output);
+				output.close();
+				cout << "Saved\n";
+			}
+
+			//Quit
+			else
+				break;
+		}
+	}
+	else
+	{
+		cout << "Invalid File Name. Restart Program.\n";
+		return 2;
+	}
 	return 0;
 }
--- two-three.h
+++ two-three.h
@@ -11,14 +11,14 @@ using namespace std;
 class TTT{
     public:
         TTT();
-        //void contains() const;
+        void contains() const;
         bool isEmpty();
-        void printTree(ostream & out = cout) const;
-        void buildTree(ifstream & input);
+        void printTreeTTT(ostream & out = cout) const;
+        void buildTreeTTT(ifstream & input);
     private:
     struct node{
-        node(const string &lv, const string &mv, const string &rv, node *l, node * m, node *r, node *p, const int h)
-        :lval(lv), mval(mv), rval(rv), left(l), middle(m), right(r), parent(p), height(h){
+        node(const string &lv, const string &mv, const string &rv, node *l, node * m, node *r, node *p)
+        :lval(lv), mval(mv), rval(rv), left(l), middle(m), right(r), parent(p){
             lines.resize(0); // Resizes vector to only allot necessary amount of space
         }
         string lval;
@@ -28,14 +28,17 @@ class TTT{
         node * middle;
         node * right;
         node * parent;
-        int height;
+
         vector<int> lines;
     };
     node * root;
     void insertHelper(const string &X, int line, node *& t, int &distWords);
-	//bool containsHelper(const string & x, node * t, node* &result) const;
+    void leftInsertHelper(node *& t, const string &X);
+    void splitInsertHelper(node *& t, const string &X);
+    char directionHelper(node *& t, node *& p);
+	bool containsHelper(const string & x, node * t, node* &result) const;
 	void printTreeHelper(node *t, ostream & out) const;
-	//int findHeight(node *t);
+	int findHeight(node *t);
 };
 
 #endif
